f7e4b48df1ea0a8ffe8bc067f6524e0bef05abaf
SayimDurmus <dur-say@hotmail.com>
2023-03-10 20:21:46 +0300

    Mod4Counter-BinaryConverter

---
+++ Mod4Sayac/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod4Sayac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3 basamaklı mod4 sayacının yapılması
+            //int sayi = 100;
+            //double birlerBas, dortlerBas, onaltilarBas, toplam;
+
+            //for (int i = 1; i < sayi; i++)
+            //{
+            //    birlerBas = (i % 4);
+            //    dortlerBas = ((((i - birlerBas) / 4) % 4));
+            //    onaltilarBas = (((((i - birlerBas) / 4) - dortlerBas) / 4));
+            //    toplam = birlerBas * Math.Pow(4, 0) + dortlerBas * Math.Pow(4, 1) + onaltilarBas * Math.Pow(4, 2);
+            //    string sonuc = $"{onaltilarBas}{dortlerBas}{birlerBas}";
+            //    string msg = $"{onaltilarBas}{dortlerBas}{birlerBas} -- {toplam}";
+            //    if (sonuc == "400")
+            //    {
+            //        break;
+            //    }
+
+            //    Console.WriteLine(msg);
+            //    birlerBas = 0;
+            //    dortlerBas = 0;
+            //    onaltilarBas = 0;
+            //    sonuc = string.Empty;
+
+            //}
+            //Console.ReadLine();
+
+            
+
+        }
+    }
+}
---
+++ _Ondalikli_Bir_Sayi_8Bit_Isaretli_Binary/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Ondalikli_Bir_Sayi_8Bit_Isaretli_Binary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ondalıklı olarak 8 bitlik ve işaretli (+ ya da -) bir sayının binary sistemde çıktısını veren uygulama 
+
+            float number;
+            float remainder, numberFirst = 0, numberLast = 0;
+            string result = "";
+            string fullNumber = string.Empty;
+            bool isNegative;
+            bool isDecimal = false;
+            Console.WriteLine("Sayı Giriniz: ");
+            number = float.Parse(Console.ReadLine());
+            if (number.ToString().Contains(","))
+            {
+                numberFirst = int.Parse(number.ToString().Split('.').First());
+                numberLast = int.Parse(number.ToString().Split('.').Last());
+                isDecimal = true;
+            }
+
+            isNegative = number < 0;
+            number = isNegative ? (number * (-1)) : number;
+
+            if (isDecimal)
+            {
+                float[] numbers = { numberFirst, numberLast };
+                for (int i = 0; i <= 1; i++)
+                {
+                    number = numbers[i];
+                    while (number >= 2)
+                    {
+                        remainder = number % 2;
+                        result = result.Insert(0, remainder.ToString());
+                        number /= 2;
+                    }
+                    result = result.Insert(0, number.ToString());
+                    result = result.PadLeft(8, '0');
+                    if (number == numbers[1])
+                        result = result.Insert(0, (isNegative ? "1" : "0"));
+
+                    fullNumber += result;
+                    Console.WriteLine("Sonuc: " + fullNumber);
+                }
+
+            }
+            else
+            {
+                while (number >= 2)
+                {
+                    remainder = number % 2;
+                    result = result.Insert(0, remainder.ToString());
+                    number /= 2;
+                }
+                result = result.Insert(0, number.ToString());
+                result = result.PadLeft(7, '0');
+                result = result.Insert(0, (isNegative ? "1" : "0"));
+                Console.WriteLine("Sonuc: " + result);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
